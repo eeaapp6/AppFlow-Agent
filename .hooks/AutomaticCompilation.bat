650fdb3ba4a4523fd657f1149f1b2467491d7972
zhonghua <13287264578@163.com>
2024-03-06 11:16:49 +0800

    钩子优化输出文件格式UTF-8

--- .hooks/AutomaticCompilation.bat
+++ .hooks/AutomaticCompilation.bat
@@ -1,6 +1,8 @@
 @echo off
 ::设置工程路径-需要设置
 ::cd code
+::执行创建sln工程
+::call .\Create_FITK_sln.bat
 ::显示当前路径
 echo %cd%
 ::变量延迟的启动语句
@@ -22,25 +24,20 @@ echo "building ...."
 ::获取vcvarsall.bat-需要设置
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
 ::编译程序设置构建模式有错误信息输出到文件msbulid.err中
-MSBuild %project% /t:Build /p:Configuration=Release /flp2:errorsonly;logfile=msbuild.err
+MSBuild %project% /t:Build /p:Configuration=Release /fl /flp:errorsonly;logfile=msbuild.err;Encoding=UTF-8
+::获取返回值
+set result=%ERRORLEVEL%
+echo %result%
 ::构建完成
 echo build complete
-::获取msbuild.err文件大小
-for /f %%i in ('dir /a-d /b msbuild.err') do ( set filesize=%%~zi )
 ::打印当前日期
 date /t
 ::打印当前时间
 time /t
-echo %cd%\msbuild.err %filesize%
+echo %cd%\msbuild.err
 pause
-::判断msbuild.err文件大小是否为0
-if %filesize%==0 (
+if %result%==0 (
     del msbuild.err
     exit 0
 )
-::判断msbuild.err文件是否有数据
-if %filesize% gtr 0 (
-    exit 1
-)
-
-exit 2
+exit 1
