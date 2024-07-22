b0055cac98840d6dc4475fc441f67f6aee97b307
程浩天 <yeguangbaozi@foxmail.com>
2024-07-22 10:56:10 +0800

    增加linux初始化pythin环境；优化拾取交互逻辑。

--- FlowApp/main.cpp
+++ FlowApp/main.cpp
@@ -31,6 +31,18 @@ int main(int argc, char *argv[])
     // 初始化应用框架
     AppFrame::FITKApplication app(argc, argv);
 
+#ifdef Q_OS_LINUX
+    qputenv("PYTHONHOME", app.applicationDirPath().toUtf8() + "/../Python37");
+
+    QStringList pythonPaths;
+    pythonPaths << app.applicationDirPath() + "/../Python37/bin";
+    pythonPaths << app.applicationDirPath() + "/../Python37/lib";
+    pythonPaths << app.applicationDirPath() + "/../Python37/lib/python3.7";
+    pythonPaths << app.applicationDirPath() + "/../Python37/lib/python3.7/site-packages";
+    pythonPaths << app.applicationDirPath() + "/../Python37/lib/python3.7/lib-dynload";
+    qputenv("PYTHONPATH", pythonPaths.join(":").toUtf8());
+#endif
+
    // 注册程序的主要组件和设置
     app.regMainWindowGenerator(new MainWindowGenerator); // 注册主窗口生成器
     app.regGlobalDataFactory(new GlobalDataFactory);     // 注册全局数据工厂
