9ef5e6632abd0051cfe969954fc857b2895ca3ae
付礼朋 <18845613215@163.com>
2024-07-25 10:11:10 +0800

    添加测试代码

--- OperatorsModel/OperatorsMeshManager.cpp
+++ OperatorsModel/OperatorsMeshManager.cpp
@@ -3,6 +3,13 @@
 #include "FITK_Component/FITKOFDictWriter/FITKOFBlockMeshDictWriter.h"
 #include "FITK_Kernel/FITKCore/FITKDirFileTools.h"
 
+#include "FITK_Kernel/FITKAppFramework/FITKAppFramework.h"
+#include "FITK_Kernel/FITKAppFramework/FITKAbstractCommandRunner.h"
+#include "FITK_Kernel/FITKAppFramework/FITKAbstractProgramDriver.h"
+#include "FITK_Kernel/FITKAppFramework/FITKProgramTaskManager.h"
+#include "FITK_Component/FITKOFDriver/FITKOFInputInfo.h"
+#include <QStringList>
+
 #include <QApplication>
 
 namespace ModelOper
@@ -22,7 +29,15 @@ namespace ModelOper
         IO::FITKOFBlockMeshDictWriter meshDickWri;
         meshDickWri.setFilePath(path);
         meshDickWri.run();
-
+        auto app = dynamic_cast<AppFrame::FITKApplication*>(qApp);
+        if (!app) return false;
+        auto proGramManager = app->getProgramTaskManager();
+        if (!proGramManager) return false;
+        AppFrame::FITKProgramInputInfo* info = new FoamDriver::FITKOFInputInfo();
+        QStringList args;
+        args << "-case" << path;
+        info->setArgs(args);
+        proGramManager->startProgram(1, "FITKOFBlockMeshDriver", info);
         return true;
     }
 
