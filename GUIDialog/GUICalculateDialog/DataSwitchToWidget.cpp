a036d07ee70dd8bf2b66a3523e9e0336b03e2f3a
BaGuijun <1549731314@qq.com>
2024-08-13 18:16:54 +0800

    求解器界面参数部分递归部分添加

--- GUIDialog/GUICalculateDialog/DataSwitchToWidget.cpp
+++ GUIDialog/GUICalculateDialog/DataSwitchToWidget.cpp
@@ -7,6 +7,11 @@
 #include "GUIWidgetString.h"
 
 #include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowDataBase.h"
+#include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowDataCombox.h"
+#include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowDataInt.h"
+#include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowDataString.h"
+#include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowDataBool.h"
+#include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowDataDouble.h"
 
 #include <QWidget>
 #include <QTableWidget>
@@ -19,7 +24,7 @@ namespace GUI
         QWidget* widget = nullptr;
         if (data == nullptr)return widget;
         Interface::FlowDataType type = data->getDataType();
-        switch (type){
+        switch (type) {
         case Interface::FlowDataType::FLowDataInt:widget = new GUIWidgetInt(data, parent); break;
         case Interface::FlowDataType::FLowDataDouble:widget = new GUIWidgetDouble(data, parent); break;
         case Interface::FlowDataType::FLowDataString:widget = new GUIWidgetString(data, parent); break;
@@ -29,7 +34,7 @@ namespace GUI
         return widget;
     }
 
-    QTableWidget* DataGroupSwitchToWidget(QWidget* parent) 
+    QTableWidget* DataGroupSwitchToWidget(QWidget* parent)
     {
         QTableWidget* widget = new QTableWidget(parent);
         //自适应布局（更具表头数量平均分配大小）
@@ -48,6 +53,34 @@ namespace GUI
 
         return widget;
     }
+
+    void DataGroupSwitchToWidget(QTableWidget* tableWidget, int& rowNum, int tierNum, Interface::FITKFlowDataGroup * dataGroup)
+    {
+        if (tableWidget == nullptr)return;
+        if (dataGroup == nullptr)return;
+        tierNum++;
+
+        for (int i = 0; i < dataGroup->getDataCount(); i++) {
+            Interface::FITKFlowDataBase* dataBase = dataGroup->getDataByIndex(i);
+            if (dataBase == nullptr)continue;
+            QWidget* widget = DataSwitchToWidget(dataBase, tableWidget);
+            if (widget == nullptr)continue;
+            tableWidget->setCellWidget(rowNum, tierNum, widget);
+            rowNum++;
+
+            switch (dataBase->getDataType()) {
+            case Interface::FlowDataType::FLowDataInt:break;
+            case Interface::FlowDataType::FLowDataDouble:break;
+            case Interface::FlowDataType::FLowDataString:break;
+            case Interface::FlowDataType::FLowDataComBox: {
+                Interface::FITKFlowDataCombox* comboxWidget = dynamic_cast<Interface::FITKFlowDataCombox*>(dataBase);
+                if(comboxWidget == nullptr)break;
+                Interface::FITKFlowDataGroup* subDataGroup = comboxWidget->getValueGroup(comboxWidget->getValue()[comboxWidget->getIndex()]);
+                DataGroupSwitchToWidget(tableWidget, rowNum, tierNum, subDataGroup);
+                break;
+            }
+            case Interface::FlowDataType::FLowDataBool:break;
+            }
+        }
+    }
 }
-
-
--- GUIDialog/GUICalculateDialog/DataSwitchToWidget.h
+++ GUIDialog/GUICalculateDialog/DataSwitchToWidget.h
@@ -8,12 +8,14 @@ class QTableWidget;
 
 namespace Interface {
     class FITKFlowDataBase;
+    class FITKFlowDataGroup;
 }
 
 namespace GUI
 {
     GUICalculateDialogAPI QWidget* DataSwitchToWidget(Interface::FITKFlowDataBase* data, QWidget* parent = nullptr);
     GUICalculateDialogAPI QTableWidget* DataGroupSwitchToWidget(QWidget* parent = nullptr);
+    void DataGroupSwitchToWidget(QTableWidget* tableWidget, int& rowNum, int tierNum, Interface::FITKFlowDataGroup* dataGroup);
 }
 
 #endif
