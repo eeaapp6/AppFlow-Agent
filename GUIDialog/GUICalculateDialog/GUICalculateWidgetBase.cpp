1fff8a5a822906d466bf9c63b9088d36b3383d60
BaGuijun <1549731314@qq.com>
2024-08-21 13:04:17 +0800

    编译失败问题修复

--- GUIDialog/GUICalculateDialog/GUICalculateWidgetBase.cpp
+++ GUIDialog/GUICalculateDialog/GUICalculateWidgetBase.cpp
@@ -3,16 +3,14 @@
 #include "FITK_Kernel/FITKAppFramework/FITKAppFramework.h"
 #include "FITK_Kernel/FITKAppFramework/FITKGlobalData.h"
 #include "FITK_Kernel/FITKAppFramework/FITKComponents.h"
-#include "FITK_Interface/FITKInterfaceFlowOF/FITKOFCasePhysicsData.h"
-#include "FITK_Interface/FITKInterfaceFlowOF/FITKFlowSolverProcessFactory.h"
 
 namespace GUI
 {
     GUICalculateWidgetBase::GUICalculateWidgetBase(EventOper::ParaWidgetInterfaceOperator * oper, QWidget * parent) :
         GUIWidgetBase(parent), _oper(oper)
     {
-        _solverData = FITKAPP->getGlobalData()->getPhysicsData<Interface::FITKOFCasePhysicsData>();
-        _factor = dynamic_cast<Interface::FITKFlowSolverProcessFactory*>(FITKAPP->getComponents()->getComponentByName("FITKFlowSolverProcess"));
+        /*_solverData = FITKAPP->getGlobalData()->getPhysicsData<Interface::FITKOFCasePhysicsData>();
+        _factor = dynamic_cast<Interface::FITKFlowSolverProcessFactory*>(FITKAPP->getComponents()->getComponentByName("FITKFlowSolverProcess"));*/
     }
 
     GUICalculateWidgetBase::~GUICalculateWidgetBase()
