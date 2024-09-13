90837a07840f62e55c4a521fc7781e43c8ab2ede
zhonghua <liuzhonghuaszch@163.com>
2024-09-13 15:04:42 +0800

    清空几何数据

--- OperatorsModel/OperatorsOpen.cpp
+++ OperatorsModel/OperatorsOpen.cpp
@@ -92,6 +92,20 @@ namespace ModelOper
         QString fileName;
         bool ok = this->argValue<QString>("FileName", fileName);
         this->clearArgs();
+        EventOper::GraphEventOperator* graphOper = FITKOPERREPO->getOperatorT<EventOper::GraphEventOperator>("GraphPreprocess");
+        if (graphOper == nullptr)return;
+        //清理几何数据
+        Interface::FITKGeoCommandList* geoCommList = FITKAPP->getGlobalData()->getGeometryData<Interface::FITKGeoCommandList>();
+        if (!geoCommList)return;
+        for (int i = geoCommList->getDataCount() - 1; i >= 0; --i)
+        {
+            auto geoComm = geoCommList->getDataByIndex(i);
+            if (!geoCommList)continue;
+            int geoId = geoComm->getDataObjectID();
+            geoCommList->removeDataObj(geoComm);
+            graphOper->updateGraph(geoId);
+        }
+        geoCommList->clear();
 
         auto fitkAbaIO = FITKAPP->getComponents()->getComponentTByName<IO::FITKFlowOFIOHDF5Interface>("FITKFlowOFHDF5IO");
         if (fitkAbaIO == nullptr) return;
@@ -119,8 +133,6 @@ namespace ModelOper
         //更新几何界面显示
         Interface::FITKGeoCommandList* geometryData = FITKAPP->getGlobalData()->getGeometryData<Interface::FITKGeoCommandList>();
         if (geometryData == nullptr) return;
-        EventOper::GraphEventOperator* graphOper = FITKOPERREPO->getOperatorT<EventOper::GraphEventOperator>("GraphPreprocess");
-        if (graphOper == nullptr)return;
         for (int i = 0; i < geometryData->getDataCount(); i++) {
             auto geoData = geometryData->getDataByIndex(i);
             if (geoData == nullptr)continue;
