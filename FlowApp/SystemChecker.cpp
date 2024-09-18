d3ed5804ad378a95a10e78bd13d2d67ab41c246f
libaojun <libaojunqd@foxmail.com>
2024-09-18 21:29:05 +0800

    添加系统检查接口

--- FlowApp/SystemChecker.cpp
+++ FlowApp/SystemChecker.cpp
@@ -4,6 +4,7 @@
 QStringList SystemChecker::check()
 {
     QStringList s;
+    //线程数检查，最小是4
     const int nThread = QThread::idealThreadCount();
     if (nThread < 4)
         s << QString("CPU thread count is %1, at least 4 !").arg(nThread);
--- FlowApp/SystemChecker.h
+++ FlowApp/SystemChecker.h
@@ -1,15 +1,43 @@
-﻿#ifndef _SYSTEM_CHECKER____H___
+﻿/**
+ * 
+ * @file SystemChecker.h
+ * @brief 系统检查器
+ * @author libaojun (libaojunqd@foxmail.com)
+ * @date 2024-09-18
+ * 
+ */
+#ifndef _SYSTEM_CHECKER____H___
 #define _SYSTEM_CHECKER____H___
  
 
 #include "FITK_Kernel/FITKAppFramework/FITKAbstractSysChecker.h"
 
+/**
+ * @brief 系统检查器
+ * @author libaojun (libaojunqd@foxmail.com)
+ * @date 2024-09-18
+ */
 class SystemChecker : public AppFrame::FITKAbstractSysChecker
 {
 public: 
+    /**
+     * @brief Construct a new System Checker object
+     * @author libaojun (libaojunqd@foxmail.com)
+     * @date 2024-09-18
+     */
     explicit SystemChecker() = default;
+    /**
+     * @brief Destroy the System Checker object
+     * @author libaojun (libaojunqd@foxmail.com)
+     * @date 2024-09-18
+     */
     virtual ~SystemChecker() = default;
-
+     /**
+         * @brief 进行检查，返回错误信息
+         * @return QStringList 错误信息
+         * @author libaojun (libaojunqd@foxmail.com)
+         * @date 2024-09-18
+         */
     virtual QStringList check() override;
 
 };
